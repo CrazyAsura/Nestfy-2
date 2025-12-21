1380d69150bf0e4e3be479484ad0cb24385a5958
Leon <leoncdzt@gmail.com>
2025-12-21 17:42:15 -0300

    fix: correct NestJS build output and start path

---
+++ backend/api/index.js
@@ -0,0 +1,39 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.bootstrap = void 0;
+const core_1 = require("@nestjs/core");
+const app_module_1 = require("../src/app.module");
+const common_1 = require("@nestjs/common");
+const swagger_1 = require("@nestjs/swagger");
+const platform_express_1 = require("@nestjs/platform-express");
+const express_1 = __importDefault(require("express"));
+const server = (0, express_1.default)();
+const bootstrap = async (expressInstance) => {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, new platform_express_1.ExpressAdapter(expressInstance));
+    const frontendUrl = process.env.FRONTEND_URL || '*';
+    app.enableCors({
+        origin: frontendUrl,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        credentials: true,
+    });
+    app.useGlobalPipes(new common_1.ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+    }));
+    const config = new swagger_1.DocumentBuilder()
+        .setTitle('Ecommerce API')
+        .setDescription('The Ecommerce API description')
+        .setVersion('1.0')
+        .addBearerAuth()
+        .build();
+    const document = swagger_1.SwaggerModule.createDocument(app, config);
+    swagger_1.SwaggerModule.setup('api', app, document);
+    await app.init();
+};
+exports.bootstrap = bootstrap;
+(0, exports.bootstrap)(server);
+exports.default = server;
